3ceffc9ee5d532fc18af726b910da0f8bfe80f1b
RunicPillow <86902739+RunicPillow@users.noreply.github.com>
2022-01-11 00:43:09 -0600

    Add Tarea

---
+++ frontend-tareas/src/components/Tarea.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h1 class="display-4 text-center">Listado de Tareas</h1>
+        <hr>
+        <div class="row">
+            <div class="col-lg-8 offset-lg-2">
+                <div class="card mt-4"> 
+                    <div class="card-body">
+                        <div class="input-group">
+                            <input type="text" class="form-control form-control-lg" placeholder="Agregar tarea">
+                            <div class="input-group-append">
+                                <button class="btn btn-success btn-lg">Agregar</button>    
+                            </div>
+                        </div>
+                        <br>
+                        <ul class="listgroup">
+                            <li class="list-group-item d-flex justify-content-between">
+                               <span class="cursor">
+                                   <i class="far fa-circle"></i>
+                               </span>
+                                Hacer la tarea
+                                <span class="text-danger cursor">
+                                    <i class="fas fa-trash-alt"></i>
+                                </span>
+                            </li>    
+                        </ul>
+                    </div>    
+                </div>      
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Tarea'
+    }
+</script>
+
+<style scoped>
+    .cursor {
+        cursor: pointer;    
+    }
+</style>
